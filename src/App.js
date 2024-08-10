bf94e1042f065f5469290f1297023035ef6371ef
Ashik <ahmedashik333@gmail.com>
2024-08-10 16:54:40 +0530

    Sixth Commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
     <div className="container">
       <div className="jumbotron mt-5">
         <h1 className="display-4">Welcome to EduPlatform</h1>
-        <p className="lead">This is an innovative education platform designed to provide modern learning solutions.</p>
+        <p className="lead">This is an innovative education platform designed to provide modern learning solutions.
+          Click here to Sign Up </p>
         <hr className="my-4" />
         <p>Click the button below to learn more:</p>
         <a className="btn btn-primary btn-lg" href="https://your-education-platform.com" role="button">Learn more</a>
