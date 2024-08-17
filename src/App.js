7de72fd6b9e540aa37f05ad4450d4b9aa8e21d7c
Ashik <ahmedashik333@gmail.com>
2024-08-17 23:50:00 +0530

    New  Commit

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function AboutPage() {
 }
 const user = {
   name: 'Ashik',
-  imageUrl: 'https://www.google.com/imgres?q=images&imgurl=https%3A%2F%2Fimages.ctfassets.net%2Fhrltx12pl8hq%2F28ECAQiPJZ78hxatLTa7Ts%2F2f695d869736ae3b0de3e56ceaca3958%2Ffree-nature-images.jpg%3Ffit%3Dfill%26w%3D1200%26h%3D630&imgrefurl=https%3A%2F%2Fwww.shutterstock.com%2Fdiscover%2Ffree-nature-images&docid=uEeA4F2Pf5UbvM&tbnid=0E5dDA82VanW3M&vet=12ahUKEwjHktSrkfqHAxUtSWwGHaWIEyYQM3oECHcQAA..i&w=1200&h=630&hcb=2&ved=2ahUKEwjHktSrkfqHAxUtSWwGHaWIEyYQM3oECHcQAA'
+  imageUrl: 'C:\Users\ASHIK\Desktop\Ashik_Documents\Ashik_PP.jpg'
 };
 
 function App() {
